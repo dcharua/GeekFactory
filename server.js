9b7a2cf332f794587012a5f3f5a42fce0f241d5b
dcharua <danielcharua@hotmail.com>
2018-09-17 21:27:31 -0500

    express2

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory  
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/geekFactory'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+
+res.sendFile(path.join(__dirname,'/dist/geekFactory/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
