a778be07012febbb1da905c37544a9dfd2e097d9
Sergio Ugalde <serch037@gmail.com>
2018-10-14 17:54:41 -0500

    Recalc ponderation on change

--- src/app/criterios/criterios.component.ts
+++ src/app/criterios/criterios.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { Criterio } from './criterio.model';
-import { Proyecto } from './proyecto.model';
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import {Criterio} from './criterio.model';
+import {Proyecto} from './proyecto.model';
 
 declare var $: any;
+
 @Component({
   selector: 'app-criterios',
   templateUrl: './criterios.component.html',
   styleUrls: ['./criterios.component.css']
 })
+
 export class CriteriosComponent implements OnInit {
   @ViewChild('proyectoNombre') proyectoNombre: ElementRef;
   @ViewChild('proyectoCosto') proyectoCosto: ElementRef;
@@ -17,6 +19,7 @@ export class CriteriosComponent implements OnInit {
   @ViewChild('ponderacionInput') ponderacionInput: ElementRef;
   help: boolean = false;
   total = 100;
+
   criterios: Criterio[] = [
     new Criterio('Duración (en meses)', 'Cuantitativo', 20),
     new Criterio('Valor presente neto', 'Cuantitativo', 20),
@@ -25,50 +28,60 @@ export class CriteriosComponent implements OnInit {
     new Criterio('Generación de tecnología propitaria', 'Cualitativo', 20)
   ];
 
-  proyectos: Proyecto[] =[ new Proyecto('GeekFactory', '10000', 'Administrador de Proyectos')];
+  proyectos: Proyecto[] = [new Proyecto('GeekFactory', '10000', 'Administrador de Proyectos')];
 
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  popAddPro(){
-    $("#addProyecto").fadeIn();
+  popAddPro() {
+    $('#addProyecto').fadeIn();
   }
 
-  closeAddPro(){
-    $("#addProyecto").fadeOut();
+  closeAddPro() {
+    $('#addProyecto').fadeOut();
   }
 
 
-  popAddCri(){
-    $("#addCriterio").fadeIn();
+  popAddCri() {
+    $('#addCriterio').fadeIn();
   }
 
-  closeAddCri(){
-    $("#addCriterio").fadeOut();
+  closeAddCri() {
+    $('#addCriterio').fadeOut();
   }
 
-  addPro(){
+  addPro() {
     this.proyectos.push(new Proyecto(this.proyectoNombre.nativeElement.value, this.proyectoCosto.nativeElement.value, this.proyectoDescripcion.nativeElement.value));
     this.closeAddPro();
-    $("#proyectos").css({'display':'none'});
-    $("#criterios").fadeIn();
+    $('#proyectos').css({'display': 'none'});
+    $('#criterios').fadeIn();
   }
 
-  addCri(){
-    let rest = this.ponderacionInput.nativeElement.value/5;
-    for (let criterio of this.criterios){
-      criterio.ponderacion =criterio.ponderacion - rest;
+  addCri() {
+    let rest = this.ponderacionInput.nativeElement.value / 5;
+    for (let criterio of this.criterios) {
+      criterio.ponderacion = criterio.ponderacion - rest;
     }
     this.criterios.push(new Criterio(this.criterioInput.nativeElement.value, this.tipoInput.nativeElement.value, this.ponderacionInput.nativeElement.value));
+    this.calcCriPond();
     this.closeAddCri();
   }
 
-  regresar(){
-    $("#criterios").css({'display':'none'});
-    $("#proyectos").fadeIn();
+  calcCriPond() {
+    let tmp = 0;
+    for (const criterio of this.criterios) {
+      tmp += criterio.ponderacion;
+    }
+    console.log(tmp);
+    this.total = tmp;
+  }
+
+  regresar() {
+    $('#criterios').css({'display': 'none'});
+    $('#proyectos').fadeIn();
   }
 
 }
