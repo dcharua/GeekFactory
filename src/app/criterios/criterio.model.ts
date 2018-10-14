aa28d91f2e42707ae9dda06db27c50dedfa6d4f9
Sergio Ugalde <serch037@gmail.com>
2018-10-14 18:39:04 -0500

    Mensaje error ponderación + Interpretación

--- src/app/criterios/criterio.model.ts
+++ src/app/criterios/criterio.model.ts
@@ -2,10 +2,17 @@ export class Criterio {
   public nombre: string;
   public tipo: string;
   public ponderacion: number;
+  public interpretacion: Interpetacion;
 
-  constructor(nombre: string, tipo: string, ponderacion: number) {
+  constructor(nombre: string, tipo: string, ponderacion: number, interpretacion: Interpetacion) {
     this.nombre = nombre;
     this.tipo = tipo;
     this.ponderacion = ponderacion;
+    this.interpretacion = interpretacion;
   }
+}
+
+export enum Interpetacion {
+  mayor,
+  menor
 }
--- src/app/criterios/criterios.component.ts
+++ src/app/criterios/criterios.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
-import {Criterio} from './criterio.model';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Criterio, Interpetacion} from './criterio.model';
 import {Proyecto} from './proyecto.model';
 
 declare var $: any;
@@ -17,15 +17,17 @@ export class CriteriosComponent implements OnInit {
   @ViewChild('criterioInput') criterioInput: ElementRef;
   @ViewChild('tipoInput') tipoInput: ElementRef;
   @ViewChild('ponderacionInput') ponderacionInput: ElementRef;
-  help: boolean = false;
+  @ViewChild('interpretacionInput') interpretacionInput: ElementRef;
+  help = false;
+  error = '';
   total = 100;
 
   criterios: Criterio[] = [
-    new Criterio('Duración (en meses)', 'Cuantitativo', 20),
-    new Criterio('Valor presente neto', 'Cuantitativo', 20),
-    new Criterio('Período de recuperación de la inversión ', 'Cuantitativo', 20),
-    new Criterio('Riesgo', 'Cualitativo', 20),
-    new Criterio('Generación de tecnología propitaria', 'Cualitativo', 20)
+    new Criterio('Duración (en meses)', 'Cuantitativo', 20, Interpetacion.mayor),
+    new Criterio('Valor presente neto', 'Cuantitativo', 20, Interpetacion.mayor),
+    new Criterio('Período de recuperación de la inversión ', 'Cuantitativo', 20, Interpetacion.mayor),
+    new Criterio('Riesgo', 'Cualitativo', 20, Interpetacion.mayor),
+    new Criterio('Generación de tecnología propitaria', 'Cualitativo', 20, Interpetacion.mayor)
   ];
 
   proyectos: Proyecto[] = [new Proyecto('GeekFactory', '10000', 'Administrador de Proyectos')];
@@ -54,18 +56,27 @@ export class CriteriosComponent implements OnInit {
   }
 
   addPro() {
-    this.proyectos.push(new Proyecto(this.proyectoNombre.nativeElement.value, this.proyectoCosto.nativeElement.value, this.proyectoDescripcion.nativeElement.value));
+    this.proyectos.push(new Proyecto(this.proyectoNombre.nativeElement.value,
+      this.proyectoCosto.nativeElement.value, this.proyectoDescripcion.nativeElement.value));
     this.closeAddPro();
     $('#proyectos').css({'display': 'none'});
     $('#criterios').fadeIn();
   }
 
   addCri() {
-    let rest = this.ponderacionInput.nativeElement.value / 5;
-    for (let criterio of this.criterios) {
+    const rest = this.ponderacionInput.nativeElement.value / 5;
+    for (const criterio of this.criterios) {
       criterio.ponderacion = criterio.ponderacion - rest;
     }
-    this.criterios.push(new Criterio(this.criterioInput.nativeElement.value, this.tipoInput.nativeElement.value, this.ponderacionInput.nativeElement.value));
+    let interpretacion: Interpetacion;
+    const ponderacion: number = +this.ponderacionInput.nativeElement.value
+    if (this.interpretacionInput.nativeElement.value === 'Mayor') {
+      interpretacion = Interpetacion.mayor;
+    } else {
+      interpretacion = Interpetacion.menor;
+    }
+    this.criterios.push(new Criterio(this.criterioInput.nativeElement.value, this.tipoInput.nativeElement.value,
+      ponderacion, interpretacion));
     this.calcCriPond();
     this.closeAddCri();
   }
@@ -77,6 +88,21 @@ export class CriteriosComponent implements OnInit {
     }
     console.log(tmp);
     this.total = tmp;
+    if (this.total === 100) {
+      this.error = '';
+      $('#error').css({'display': 'none'});
+      console.log('Ok');
+    }
+    if (this.total < 100) {
+      this.error = 'La suma de ponderaciones es menor a 100';
+      $('#error').css({'display': 'inherit'});
+      console.log('Too low');
+    }
+    if (this.total > 100) {
+      this.error = 'La suma de ponderaciones es mayor a 100';
+      $('#error').css({'display': 'inherit'});
+      console.log('Too high');
+    }
   }
 
   regresar() {
